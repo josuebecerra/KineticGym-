6523e0573d9a72562b177736e8bce6bdbdf17d78
jbecerra <jbecerra@crnova.com>
2026-04-05 23:21:26 -0600

    Fix: Hardcode Firebase Variables to fix iOS App Black Screen (Action missing env variables)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyAKDpbwmCgl3A8D_k6ifgI0525adApGBHs',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'kinetic-647bb.firebaseapp.com',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'kinetic-647bb',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'kinetic-647bb.firebasestorage.app',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '119582380691',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:119582380691:web:65a1bb9b7687eb59cbf366',
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || 'G-999LHFFLKR'
 };
 
 const app = initializeApp(firebaseConfig);
